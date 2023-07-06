d1467bd950ba7c0103a2893d76f21d775a9b606b
Your Name <20520490@ms.uit.edu.vn>
2023-07-06 23:13:56 +0700

    Delete ocr,yolov7 model

--- app.py
+++ app.py
@@ -4,9 +4,6 @@ import cv2
 import glob
 import json
 import os
-
-import pytesseract
-import layoutparser as lp
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
@@ -15,71 +12,6 @@ from PIL import Image
 app = Flask(__name__)
 
 
-def ocr_folder(list_img):
-	ocr_agent = lp.TesseractAgent.with_tesseract_executable('/usr/bin/tesseract')   
-	all_res = []
-	for img in list_img:
-		image = cv2.imread(img)
-		text = pytesseract.image_to_string(img,lang='vie')
-		all_res.append(text.replace("\n", "<br>"))
-	return all_res
-
-# def pdf_to_image(dir, save_dir):
-# 	files_pdf = glob.glob(dir + "/*.pdf")
-# 	folder_name = save_dir
-# 	if not os.path.exists(folder_name):
-# 		os.makedirs(folder_name)
-# 	for file in files_pdf:
-# 		images = convert_from_path(file)
-# 		print(len(images))
-# 		for i in range(len(images)):
-# 		# Save pages as images in the pdf
-# 			#images[i].save(file.split(".")[0] + "_"  + str(i) +'.jpg', 'JPEG')
-# 			img_name = file.split("/")[-1]
-# 			# images[i] = cv2.resize(images[i],(1654-1,2338-1))
-# 			# images[i].save(save_dir + "/" + img_name.split(".")[0] + "_"  + str(i) +'.jpg', 'JPEG')
-# 			image = np.array(images[i]) # convert PIL image to NumPy array
-# 			image = cv2.resize(image, (1654, 2338))
-# 			cv2.imwrite(save_dir + "/" + img_name.split(".")[0] + "_"  + str(i) +'.jpg', image)
-
-
-def upload_files(folder_name,id_file):
-	uploaded_files = request.files.getlist(id_file)
-	count_pdf = 0
-	for file in uploaded_files:
-		filename = file.filename
-		if filename.split(".")[-1] == "pdf":
-			if not os.path.exists(folder_name + "/pdf"):
-				os.makedirs(folder_name + "/pdf")
-			file.save(os.path.join(folder_name + "/pdf", filename))
-			count_pdf+=1
-		else:
-			if not os.path.exists(folder_name + "/images"):
-				os.makedirs(folder_name + "/images")
-			#image = Image.open(file)
-			#resized_image = image.resize((new_width, new_height))
-			#resized_image.save(os.path.join(folder_name + "/images", filename)) 
-			file.save(os.path.join(folder_name + "/images", filename))
-	if count_pdf > 0:
-		pdf_to_image("static/pdf","static/images")
-
-# def remove_line(crop_img):
-# 	# Remove black line
-# 	gray = cv2.cvtColor(crop_img,cv2.COLOR_BGR2GRAY)
-# 	thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
-# 	# Horizontal lines
-# 	horizontal_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (25, 1))
-# 	detected_horizontal_lines = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, horizontal_kernel, iterations=2)
-
-# 	# Vertical lines
-# 	vertical_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, 25))
-# 	detected_vertical_lines = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, vertical_kernel, iterations=2)
-
-# 	# Combine horizontal and vertical lines
-# 	detected_lines = cv2.bitwise_or(detected_horizontal_lines, detected_vertical_lines)
-# 	indices = np.where(detected_lines != 0)
-# 	crop_img[indices] = (255, 255, 255)
-# 	return crop_img
 
 @app.route('/', methods=['Get'])
 def index():
@@ -123,32 +55,7 @@ def home():
 
 @app.route('/result', methods=['POST'])
 def crop_detect():
-	#global image_path, width, height
-	global img_cropped
-	img_cropped = []
-	folder_name = "static/crop_images"
-	if not os.path.exists(folder_name):
-		os.makedirs(folder_name)
-	#try:
-	all_polygons = json.loads(request.form['myList'])
-	print("image_path:", image_path)
-	for i in range(len(image_path)):
-		try:
-			for	j in range(len(all_polygons[i])):
-				print(all_polygons[i][j])
-				x1,x2,y1,y2 = list(map(lambda x: x * ratio, all_polygons[i][j].values()))
-				img = cv2.imread(image_path[i])
-				img = cv2.resize(img,(width*ratio, height*ratio))
-				crop_img = img[y1:y2,x1:x2]
-				out_name = folder_name + "/" + image_path[i].split("/")[-1].split(".")[0] + "_" + str(j) + ".jpg"
-				img_cropped.append(out_name)
-		
-				cv2.imwrite(out_name, crop_img)
-		except ValueError as e:
-			print("Error:", e)
-
-	res = ocr_folder(img_cropped)
-	return render_template("result.html",image0 = img_cropped[0], image_path=img_cropped , res0 = res[0], res = res)
+	return render_template("result.html")
 
 
 @app.route('/', methods=['POST'])
